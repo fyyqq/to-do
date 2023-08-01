d648ad7abf2456358c538c0dcea001d8ba2d099e
unknown <afiqakimy123@gmail.com>
2023-08-01 21:54:20 +0800

    test commit

---
+++ script.js
@@ -0,0 +1,132 @@
+$('.lightMode').css('display', 'none');
+
+$(document).ready(() => {
+    $('.darkMode').click(() => {
+        $('body').addClass('mode-color');
+        $('.lightMode').css('display', 'grid');
+        $(this).css('display', 'none');
+    });
+});
+
+$(document).ready(() => {
+    $('.lightMode').click(() => {
+        $('body').removeClass('mode-color');
+        $('.lightMode').css('display', 'none');
+    });
+});
+
+$(document).ready(() => {
+    $('.btn-explore').on("mousemove", function(e) {
+        e.preventDefault();
+        const parentOffset = $(this).offset(),
+        relX = e.pageX - parentOffset.left,
+        relY = e.pageY - parentOffset.top;
+        $(this).find("span").css({
+            top : relY,
+            left : relX,
+        });
+    });
+});
+
+function myForm() {
+    var x = $('.input-text').val();
+    if (x === "") {
+        alert(`Please fill text first`);
+    } else {
+        $('tr').append(`
+            <td class="d-flex justify-content-between align-items-center list-parent">
+                <input type="text" name="text" id="text" class="list" value="${x}" readonly>
+                <div class="btn-list">
+                    <button class="btn btn-success" data-bs-target="tooltip" title="Save List" id="fa-save">
+                        <i class="fa-solid fa-arrow-down"></i>
+                    </button>
+                    <button class="btn btn-primary" data-bs-target="tooltip" title="Edit List" id="fa-pen">
+                        <i class="fa-solid fa-pen"></i>
+                    </button>
+                    <button class="btn btn-danger" data-bs-target="tooltip" title="Delete List" id="fa-trash">
+                        <i class="fa-solid fa-trash"></i>
+                    </button>
+                </div>
+            </td>`);
+    }
+    return $('.input-text').val('');
+}
+
+function myButton() {
+    const editBtn = document.querySelectorAll('#fa-pen');
+    const saveBtn = document.querySelectorAll('#fa-save');
+    const delBtn = document.querySelectorAll('#fa-trash');
+    editBtn.forEach(btn => {
+        btn.addEventListener('click', e => {
+            const each = e.currentTarget;
+            each.style.display = 'none';
+            const inputAttr = each.parentElement.parentElement.childNodes[1];
+            inputAttr.removeAttribute("readonly");
+            $(inputAttr).focus();
+            if (each) {
+                const save = each.parentElement.childNodes[1];
+                $(save).css('display', 'inline-block');
+            }
+        });
+    });
+    saveBtn.forEach(btn => {
+        btn.addEventListener('click', e => {
+            const each = e.currentTarget;
+            each.style.display = 'none';
+            const inputAttr = each.parentElement.parentElement.childNodes[1];
+            inputAttr.setAttribute("readonly", "");
+            if (each) {
+                const edit = each.parentElement.childNodes[3];
+                $(edit).css('display', 'inline-block');
+            }
+        });
+    });
+    delBtn.forEach(btn => {
+        btn.addEventListener('click', e => {
+            const each = e.currentTarget;
+            const parent = each.parentElement.parentElement;
+            $(parent).remove();
+        });
+    });
+}
+
+function deleteAll() {
+    $('#delAll').click(() => {
+        const allList = document.querySelectorAll('.list-parent');
+        console.log(allList);
+    });
+}
+
+$(document).ready(() => {
+    $('.form-text').submit(() => {
+        myForm();
+        myButton();
+        deleteAll();
+    });
+});
+
+$(document).ready(() => {
+    $('.save-btn').click(() => {
+        myForm();
+        myButton();
+        deleteAll();
+    });
+});
+
+$(document).ready(() => {
+    $('.input-text').on({
+        'focus' : function() {
+            $(this).css({
+                'width': '400px',
+                'transition': '.4s ease-out',
+                'border': '2px solid red'
+            });
+        },
+        'blur' : function() {
+            $(this).css({
+                'width': '250px',
+                'transition': '.4s ease-out'
+            });
+        }
+    });
+});
